097b852f9d070a5be19cc8cff9e14389157eb1b5
Dan Wilkinson <dan@iostudios.co.uk>
2014-01-21 19:03:30 +0000

    Added alerts for long/lat

--- js/index.js
+++ js/index.js
@@ -65,14 +65,45 @@ var app = {
             audio.play();            
             
             
+            
+            // onSuccess Geolocation
+            //
+            function onSuccess(position) {
+                //var element = document.getElementById('geolocation');
+                //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                //                    'Longitude: '          + position.coords.longitude             + '<br />' +
+                //                    'Altitude: '           + position.coords.altitude              + '<br />' +
+                //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                //                    'Heading: '            + position.coords.heading               + '<br />' +
+                //                    'Speed: '              + position.coords.speed                 + '<br />' +
+                //                    'Timestamp: '          + position.timestamp                    + '<br />';
+            
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+            
+            }
+        
+            // onError Callback receives a PositionError object
+            //
+            function onError(error) {
+                alert('code: '    + error.code    + '\n' +
+                      'message: ' + error.message + '\n');
+            }            
+            
+            
+            
+            
             //Getting ready to post to API        
             var dateNow = new Date();
             //var userId = "893";
             var userId = result.text;
-            var latitude = "52.292742";
-            var longitude = "-1.949225";
+            //var latitude = "52.292742";
+            //var longitude = "-1.949225";
             
             alert(userId);
+            alert(latitude);
+            alert(longitude);
             
             // Posting to API
             $.ajax({
@@ -127,23 +158,4 @@ var app = {
     
 };
 
-    // onSuccess Geolocation
-    //
-    function onSuccess(position) {
-        //var element = document.getElementById('geolocation');
-        //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-        //                    'Longitude: '          + position.coords.longitude             + '<br />' +
-        //                    'Altitude: '           + position.coords.altitude              + '<br />' +
-        //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
-        //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-        //                    'Heading: '            + position.coords.heading               + '<br />' +
-        //                    'Speed: '              + position.coords.speed                 + '<br />' +
-        //                    'Timestamp: '          + position.timestamp                    + '<br />';
-    }
 
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
