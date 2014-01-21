1c47ded078314a2b4aa5e829e9cc4a6dbd4be4f2
Dan Wilkinson <dan@iostudios.co.uk>
2014-01-21 21:00:26 +0000

    Added GPS to innerhtml

--- js/index.js
+++ js/index.js
@@ -137,24 +137,22 @@ var app = {
     
 };
 
-var latitude;
-var longitude;
 
 // onSuccess Geolocation
 //
 function onSuccess(position) {
-    //var element = document.getElementById('geolocation');
-    //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-    //                    'Longitude: '          + position.coords.longitude             + '<br />' +
-    //                    'Altitude: '           + position.coords.altitude              + '<br />' +
-    //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
-    //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-    //                    'Heading: '            + position.coords.heading               + '<br />' +
-    //                    'Speed: '              + position.coords.speed                 + '<br />' +
-    //                    'Timestamp: '          + position.timestamp                    + '<br />';
+    var element = document.getElementById('geolocation');
+    /element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                        'Longitude: '          + position.coords.longitude             + '<br />' +
+                        'Altitude: '           + position.coords.altitude              + '<br />' +
+                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                        'Heading: '            + position.coords.heading               + '<br />' +
+                        'Speed: '              + position.coords.speed                 + '<br />' +
+                        'Timestamp: '          + position.timestamp                    + '<br />';
 
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
 }
 
