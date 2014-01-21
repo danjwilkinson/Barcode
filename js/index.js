746a8f9d0e19e7a163babf2f7d8ada232442dc7e
Dan Wilkinson <dan@iostudios.co.uk>
2014-01-21 21:04:09 +0000

    typo

--- js/index.js
+++ js/index.js
@@ -142,7 +142,7 @@ var app = {
 //
 function onSuccess(position) {
     var element = document.getElementById('geolocation');
-    /element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                         'Longitude: '          + position.coords.longitude             + '<br />' +
                         'Altitude: '           + position.coords.altitude              + '<br />' +
                         'Accuracy: '           + position.coords.accuracy              + '<br />' +
