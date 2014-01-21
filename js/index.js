cfd9a9542ec9013e678c97dece4bf7d978856135
Dan Wilkinson <dan@iostudios.co.uk>
2014-01-21 19:26:00 +0000

    >

--- js/index.js
+++ js/index.js
@@ -135,28 +135,27 @@ var app = {
 
     //}
     
-    
-    var latitude;
-    var longitude;
-    
-    // onSuccess Geolocation
-    //
-    function onSuccess(position) {
-        //var element = document.getElementById('geolocation');
-        //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-        //                    'Longitude: '          + position.coords.longitude             + '<br />' +
-        //                    'Altitude: '           + position.coords.altitude              + '<br />' +
-        //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
-        //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-        //                    'Heading: '            + position.coords.heading               + '<br />' +
-        //                    'Speed: '              + position.coords.speed                 + '<br />' +
-        //                    'Timestamp: '          + position.timestamp                    + '<br />';
-    
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-    
-    }    
-    
 };
+
+var latitude;
+var longitude;
+
+// onSuccess Geolocation
+//
+function onSuccess(position) {
+    //var element = document.getElementById('geolocation');
+    //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+    //                    'Longitude: '          + position.coords.longitude             + '<br />' +
+    //                    'Altitude: '           + position.coords.altitude              + '<br />' +
+    //                    'Accuracy: '           + position.coords.accuracy              + '<br />' +
+    //                    'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+    //                    'Heading: '            + position.coords.heading               + '<br />' +
+    //                    'Speed: '              + position.coords.speed                 + '<br />' +
+    //                    'Timestamp: '          + position.timestamp                    + '<br />';
+
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+}
 
 
